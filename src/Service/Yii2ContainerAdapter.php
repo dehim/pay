0017fa990ec6b1022f9e89b554d8a39fa1916b87
dehim <dehim@msn.com>
2023-08-11 11:24:19 +0800

    增加yiiContainer

---
+++ src/Service/Yii2ContainerAdapter.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Yansongda\Pay\Service;
+
+use Psr\Container\ContainerInterface;
+use yii\di\Container as YiiContainer;
+use Yansongda\Pay\Exception\ContainerNotFoundException;
+
+
+class Yii2ContainerAdapter implements ContainerInterface
+{
+    protected $yiiContainer;
+
+    public function __construct(YiiContainer $yiiContainer)
+    {
+        $this->yiiContainer = $yiiContainer;
+    }
+
+    public function get($id)
+    {
+        if (!$this->yiiContainer->has($id)) {
+            throw new ContainerNotFoundException($id);
+        }
+
+        return $this->yiiContainer->get($id);
+
+        // if ($this->yiiContainer->has($id)) {
+
+        //     return $this->yiiContainer->get($id);
+        // }
+    }
+
+    // Psr\Container\ContainerInterface 没有定义 set 方法
+    public function set($id, $value): void
+    {
+        $this->yiiContainer->set($id, $value);
+    }
+
+    public function has(string $id): bool
+    {
+        return $this->yiiContainer->has($id);
+    }
+}
